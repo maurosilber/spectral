eb6ff498a5b0e95cdc2423a360010d886e1f80ba
Mauro Silberberg <maurosilber@gmail.com>
2024-02-28 17:44:38 -0300

    Update denoisers to handle mean and variance

--- src/spectral/binlets/independent.py
+++ src/spectral/binlets/independent.py
@@ -47,6 +47,7 @@ def binlets_poisson(
 def binlets_independent_components(
     A: xarray.DataArray,
     b: xarray.DataArray,
+    b_var: xarray.DataArray,
     /,
     *,
     sigma: float,
@@ -60,8 +61,12 @@ def binlets_independent_components(
 
     A = A.transpose(common_dim, *A_shape.keys())
     b = b.transpose(*b_shape.keys(), common_dim)
-    b_vector = b.values[..., None]
-    p, cov = _weighted_least_squares(A.values, b_vector, b_vector)
+    b_var = b_var.transpose(*b_shape.keys(), common_dim)
+    p, cov = _weighted_least_squares(
+        A.values,
+        b.values[..., None],
+        b_var.values[..., None],
+    )
 
     mean_and_var_shape = A_shape | b_shape
     for k in A_shape.keys():
@@ -89,3 +94,68 @@ def binlets_independent_components(
         coords=coords,
         dims=list(mean_and_var_shape.keys()),
     )
+
+
+def _binlets_variance(
+    data: np.ndarray,
+    variance: np.ndarray,
+    *,
+    sigma: float,
+    independent: bool,
+    levels: int | None = None,
+):
+    def test(x: np.ndarray, y: np.ndarray):
+        d = x[0::2] - y[0::2]
+        v = x[1::2] + y[1::2]
+        mask = np.empty(x.shape, dtype=bool)
+        m = d**2 < sigma**2 * v
+        if not independent:
+            m = m.all(0)
+        mask[0::2] = mask[1::2] = m
+        return mask
+
+    mean_and_var = np.empty((2 * data.shape[0], *data.shape[1:]), dtype=data.dtype)
+    mean_and_var[0::2] = data
+    mean_and_var[1::2] = variance
+    mean_and_var = binlets(mean_and_var, levels=levels, test=test, linear=True)
+    return mean_and_var[0::2], mean_and_var[1::2]
+
+
+def binlets_variance(
+    data: xarray.DataArray,
+    variance: xarray.DataArray,
+    /,
+    *,
+    sigma: float,
+    levels: int | None = None,
+    dim: str | None,
+    independent: bool,
+):
+    if dim is None:
+        dim = "dummy"
+        if dim in data.dims:
+            raise RuntimeError("data already has a dummy dimension")
+
+        return binlets_variance(
+            data.expand_dims(dim),
+            variance.expand_dims(dim),
+            sigma=sigma,
+            levels=levels,
+            dim=dim,
+            independent=independent,
+        ).isel({dim: 0})
+
+    dims = set(data.dims)
+    try:
+        dims.remove(dim)
+    except KeyError:
+        raise ValueError("dim is not a dimension of data")
+    dims = [dim, *dims]
+    denoised, _ = _binlets_variance(
+        data.transpose(*dims).values,
+        variance.transpose(*dims).values,
+        sigma=sigma,
+        levels=levels,
+        independent=independent,
+    )
+    return xarray.DataArray(denoised, dims=dims, coords=data.coords)
--- src/spectral/denoisers.py
+++ src/spectral/denoisers.py
@@ -4,13 +4,14 @@ import xarray
 
 from . import Dim, unmixing
 from .binlets.components import binlets_components_transform
-from .binlets.independent import binlets_independent_components, binlets_poisson
+from .binlets.independent import binlets_independent_components, binlets_variance
 
 
 class Denoiser(Protocol):
     def __call__(
         self,
         spectrum: xarray.DataArray,
+        variance: xarray.DataArray,
         *,
         sigma: float,
         calibration: xarray.DataArray,
@@ -20,69 +21,78 @@ class Denoiser(Protocol):
 
 def nothing(
     spectrum: xarray.DataArray,
+    variance: xarray.DataArray,
     *,
     sigma: float,
     calibration: xarray.DataArray,
 ):
-    return unmixing.weighted_least_squares(calibration, spectrum)
+    return unmixing.weighted_least_squares(calibration, spectrum, variance)
 
 
 def single_spectrum(
     spectrum: xarray.DataArray,
+    variance: xarray.DataArray,
     *,
     sigma: float,
     calibration: xarray.DataArray,
 ):
-    spectrum = binlets_poisson(
+    spectrum = binlets_variance(
         spectrum,
+        variance,
         sigma=sigma,
         dim=Dim.spectrum,
         independent=True,
     )
-    return unmixing.weighted_least_squares(calibration, spectrum)
+    return unmixing.weighted_least_squares(calibration, spectrum, variance)
 
 
 def full_spectrum(
     spectrum: xarray.DataArray,
+    variance: xarray.DataArray,
     *,
     sigma: float,
     calibration: xarray.DataArray,
 ):
-    spectrum = binlets_poisson(
+    spectrum = binlets_variance(
         spectrum,
+        variance,
         sigma=sigma,
         dim=Dim.spectrum,
         independent=False,
     )
-    return unmixing.weighted_least_squares(calibration, spectrum)
+    return unmixing.weighted_least_squares(calibration, spectrum, variance)
 
 
 def continuous_spectrum(
     spectrum: xarray.DataArray,
+    variance: xarray.DataArray,
     *,
     sigma: float,
     calibration: xarray.DataArray,
 ):
-    spectrum = binlets_poisson(
+    spectrum = binlets_variance(
         spectrum,
+        variance,
         sigma=sigma,
         dim=None,
         independent=True,  # irrelevant as len(dim=None) == 1
     )
-    return unmixing.weighted_least_squares(calibration, spectrum)
+    return unmixing.weighted_least_squares(calibration, spectrum, variance)
 
 
 def single_component(
     spectrum: xarray.DataArray,
+    variance: xarray.DataArray,
     *,
     sigma: float,
     calibration: xarray.DataArray,
 ):
-    return binlets_independent_components(calibration, spectrum, sigma=sigma)
+    return binlets_independent_components(calibration, spectrum, variance, sigma=sigma)
 
 
 def full_component(
     spectrum: xarray.DataArray,
+    variance: xarray.DataArray,
     *,
     sigma: float,
     calibration: xarray.DataArray,
@@ -93,4 +103,4 @@ def full_component(
         dim=Dim.spectrum,
         calibration=calibration,
     )
-    return unmixing.weighted_least_squares(calibration, spectrum)
+    return unmixing.weighted_least_squares(calibration, spectrum, variance)
